cce48f188920fb9206496ed980a10b87d9fc6fda
ansh <anshkapilmehta@gmail.com>
2024-03-10 13:50:23 +0530

    Landing Page complete UI

---
+++ src/components/welcome.component.jsx
@@ -0,0 +1,30 @@
+// eslint-disable-next-line react/prop-types
+function WelcomeBox({ theme }) {
+  return (
+    <section className="flex flex-row items-center justify-center mt-10 md:mt-6 w-3/5">
+      <img
+        className="size-10 md:size-12 mt-5  z-10 left-0"
+        src={
+          theme === "light"
+            ? "../src/images/scribble_bottom_dark.svg"
+            : "../src/images/scribble_bottom_light.svg"
+        }
+        alt="scribble"
+      />
+      <h2 className="text-center rounded-3xl px-4 py-2 italic md:text-2xl font-elephant bg-black dark:bg-light-main text-light-main dark:text-black relative z-20">
+        Welcomes You
+      </h2>
+      <img
+        className="size-10 md:size-12 mb-5  z-10 right-0"
+        src={
+          theme === "light"
+            ? "../src/images/scribble_top_dark.svg"
+            : "../src/images/scribble_top_light.svg"
+        }
+        alt="scribble"
+      />
+    </section>
+  );
+}
+
+export default WelcomeBox;
--- src/routes/landing/landing.component.jsx
+++ src/routes/landing/landing.component.jsx
@@ -1,9 +1,65 @@
-const Landing = () => {
+import { React, useEffect, useState } from "react";
+import Heading from "../../components/heading.component";
+import WelcomeBox from "../../components/welcome.component";
+
+function Landing() {
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const setThemeFromSystem = () => {
+      if (mediaQuery.matches) {
+        document.documentElement.classList.add("dark");
+        setTheme("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        setTheme("light");
+      }
+    };
+
+    // Set theme from system preference on load
+    setThemeFromSystem();
+
+    // Update theme when system preference changes
+    mediaQuery.addEventListener("change", setThemeFromSystem);
+
+    // Clean up event listener
+    return () => {
+      mediaQuery.removeEventListener("change", setThemeFromSystem);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <>
+      <main className="min-h-screen h-full min-w-full bg-light-main pt-10 flex flex-col items-center dark:bg-black">
+        <Heading text="IEEE-CS" />
+        <WelcomeBox theme={theme} />
+        <section className="flex flex-col w-4/5 mt-10 md:w-1/2 md:mt-5 ">
+          <h1 className="text-4xl md:text-5xl text-center font-elephant text-black dark:text-light-main">
+            Empowering seekers of treasure
+          </h1>
+          <p className="text-sub-text font-outfit text-xl md:text-2xl text-center mt-10 md:mt-6">
+            Experience our IEEE Computer Society (IEEECS) chapter, where
+            innovation meets exploration. Join us as we delve into the exciting
+            world of technology, where we empower seekers like you to unearth
+            technological treasures.
+          </p>
+        </section>
+        <section className="flex flex-row items-center absolute bottom-6 w-8/12">
+          <hr className="hidden md:block flex-grow border-2 border-black dark:border-light-main  h-px" />
+          <img
+            src={
+              theme === "light"
+                ? "../src/images/google_light.svg"
+                : "../src/images/google_dark.svg"
+            }
+            alt="description"
+            className="mx-6 w-72 h-auto"
+          />
+          <hr className="hidden md:block flex-grow border-2 border-black  dark:border-light-main h-px" />
+        </section>
+      </main>
+    </>
   );
-};
+}
 
 export default Landing;
